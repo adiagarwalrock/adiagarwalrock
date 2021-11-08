7862b45a191f639ccd68bf4fa80e16f97aa87384
Aditya Agarwal <aditya.ag1234@gmail.com>
2021-11-09 03:31:58 +0530

    Fixed js

--- static/js/main.js
+++ static/js/main.js
@@ -1,64 +1,29 @@
-/*===== MENU SHOW =====*/
-const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
 
-  if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      nav.classList.toggle("show");
-    });
-  }
-};
-showMenu("nav-toggle", "nav-menu");
+/*SCROLL HOME*/
+sr.reveal(".home__title", {});
+sr.reveal(".home__img", { delay: 200 });
+sr.reveal(".home__social-icon", { interval: 200 });
 
-// /*===== ACTIVE AND REMOVE MENU =====*/
-// const navLink = document.querySelectorAll(".nav__link");
-
-// function linkAction() {
-//   /*Active link*/
-//   navLink.forEach((n) => n.classList.remove("active"));
-//   this.classList.add("active");
-
-//   /*Remove menu mobile*/
-//   const navMenu = document.getElementById("nav-menu");
-//   navMenu.classList.remove("show");
-// }
-// navLink.forEach((n) => n.addEventListener("click", linkAction));
-
-// /*===== SCROLL REVEAL ANIMATION =====*/
-// const sr = ScrollReveal({
-//   origin: "top",
-//   distance: "80px",
-//   duration: 2000,
-//   reset: true,
-// });
-
-// /*SCROLL HOME*/
-// sr.reveal(".home__title", {});
-// sr.reveal(".button", { delay: 200 });
-// sr.reveal(".home__img", { delay: 400 });
-// sr.reveal(".home__social-icon", { interval: 200 });
-
-// /*SCROLL ABOUT*/
+/*SCROLL ABOUT*/
 // sr.reveal(".about__img", {});
 // sr.reveal(".about__subtitle", { delay: 400 });
-// sr.reveal(".about__text", { delay: 400 });
+sr.reveal(".about__text", { delay: 400 });
 
-// /*SCROLL SKILLS*/
-// sr.reveal(".skills__subtitle", {});
-// sr.reveal(".skills__text", {});
-// sr.reveal(".skills__data", { interval: 200 });
-// sr.reveal(".skills__img", { delay: 600 });
+/*SCROLL SKILLS*/
+sr.reveal(".all__skills", { interval: 200 });
 
-// /*SCROLL PROJECTS*/
-// sr.reveal(".projects__img", { interval: 200 });
+/*SCROLL PROJECTS*/
+sr.reveal(".project__card", { interval: 200 });
 
-// /*SCROLL CONTACT*/
-// sr.reveal(".contact__input", {});
-
-/*SCROLL TIMELINE*/
-// sr.reveal(".timeline", { interval: 200 });
-// sr.reveal(".content", { interval: 400 });
+/*SCROLL CERTIFICATES*/
+sr.reveal(".certifi__all", { interval: 200 });
 
 
 function countChar(val) {
