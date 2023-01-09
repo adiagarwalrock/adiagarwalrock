4692a34f76ac099172ca057b59fc3a4518960f56
Aditya Agarwal <aditya.ag1234@gmail.com>
2023-01-09 03:00:15 -0500

    Final Changes V4

--- app.py
+++ app.py
@@ -12,49 +12,43 @@ load_dotenv(dotenv_path=ENV_PATH)
 
 
 app = Flask(__name__)
-# app.secret_key = os.environ['APP_SECRET']
-app.secret_key = "qwertyuioasdfghjklzxcvbnm,"
+app.secret_key = os.environ['APP_SECRET']
+# app.secret_key = "qwertyuioasdfghjklzxcvbnm,"
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     ip_address = request.remote_addr
-    # slack_bot = Bot()
-    # message="ACCESSED IP: " + ip_address
     return index_page_view()
 
 
-# @app.route('/resume/', methods=['GET', 'POST'])
-# def resume_page():
-#     return render_template("resume.html")
-
-
-# @app.route(os.environ['ADMIN_URL'], methods=['GET', 'POST'])
-@app.route('/potato/', methods=['GET', 'POST'])
+@app.route(os.environ['ADMIN_URL'], methods=['GET', 'POST'])
+# @app.route('/potato/', methods=['GET', 'POST'])
 def admin_page():
     return admin_page_view()
 
 
-# @app.route(os.environ['ADMIN_URL']+'success/', methods=['GET', 'POST'])
-@app.route('/potato/success/', methods=['GET', 'POST'])
+@app.route(os.environ['ADMIN_URL']+'success/', methods=['GET', 'POST'])
+# @app.route('/potato/success/', methods=['GET'])
 def success():
     return redirect(url_for('admin_page'))
 
 
-@app.route('/legacy/index/', methods=['GET', 'POST'])
-# Old Homepage Design
-def legacy():
-    return render_template("/legacy/index.html")
+# @app.route('/legacy/index/', methods=['GET'])
+# # Old Homepage Design
+# def legacy():
+#     return render_template("/legacy/index.html")
 
 
-@app.route('/feature/', methods=['GET', 'POST'])
-def feature():
-    return render_template("/features.html")
+@app.route('/resume/', methods=['GET'])
+def resume():
+    return render_template("resume.html")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('/features.html')
+    return render_template('/error.html')
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
