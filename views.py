a6db6fd16de3b7b06cf33d6875dedbe272dc123d
Aditya Agarwal <aditya.ag1234@gmail.com>
2024-02-10 04:53:15 -0500

    Update resume template and landing page projects

--- views.py
+++ views.py
@@ -10,13 +10,12 @@ from pathlib import Path
 from bot import Github_Bot
 
 
-ENV_PATH = Path('.') / '.env'
+ENV_PATH = Path(".") / ".env"
 load_dotenv(dotenv_path=ENV_PATH)
 
 
-
-FAV_PROJ_DB_PATH = 'static/db/landing_page_projects.csv'
-CERT_DB_PATH = 'static/db/certs.csv'
+FAV_PROJ_DB_PATH = "static/db/landing_page_projects.csv"
+CERT_DB_PATH = "static/db/certs.csv"
 SKILLS_PATH = "data.json"
 
 
@@ -24,38 +23,36 @@ def admin_page_view():
     github_bot = Github_Bot()
     github_repos = github_bot.get_public_repo()
 
-    if request.method == 'POST':
-        if request.form.get('landing_page_submit'):
+    if request.method == "POST":
+        if request.form.get("landing_page_submit"):
             # Edit Fav Projects to pin
-            landing_page_items = request.form.getlist('landing_page')
+            landing_page_items = request.form.getlist("landing_page")
 
-            with open(FAV_PROJ_DB_PATH, 'w', newline='') as file:
+            with open(FAV_PROJ_DB_PATH, "w", newline="") as file:
                 writer = csv.writer(file)
                 for item in range(len(landing_page_items)):
                     writer.writerow([landing_page_items[item]])
 
-        if request.form.get('skill_submit'):
+        if request.form.get("skill_submit"):
             # Add New Skill
-            new_skill = request.form.get('new_skill')
-            skill_category = request.form.get('skill_category')
-            with open("data.json", "r+") as jsonfile:
+            new_skill = request.form.get("new_skill")
+            skill_category = request.form.get("skill_category")
+            with open(SKILLS_PATH, "r+") as jsonfile:
                 data = json.load(jsonfile)
-                data['skills'][skill_category].append(new_skill)
+                data["skills"]["skills"][skill_category].append(new_skill)
                 jsonfile.truncate(0)
                 jsonfile.seek(0)
                 json.dump(data, jsonfile)
 
-        return redirect(url_for('success'))
+        return redirect(url_for("success"))
 
-    context = {
-        "content": github_repos
-    }
+    context = {"content": github_repos}
     return render_template("admin.html", **context)
 
 
 def index_page_view():
 
-    f = open(SKILLS_PATH, 'r')
+    f = open(SKILLS_PATH, "r")
     data = json.load(f)
     f.close()
 
@@ -63,31 +60,30 @@ def index_page_view():
     github_repos = github_bot.get_public_repo()
 
     projects_data = []
-    with open(FAV_PROJ_DB_PATH, 'r') as file:
+    with open(FAV_PROJ_DB_PATH, "r") as file:
         reader = csv.reader(file)
         for row in reader:
             projects_data.append(row[0])
 
     cert_data = []
-    with open(CERT_DB_PATH, 'r') as file:
+    with open(CERT_DB_PATH, "r") as file:
         reader = csv.reader(file)
         for row in reader:
             cert_data.append(row)
 
-    if request.method == 'POST':
-        name = str(request.form['name'])
-        email = str(request.form['email'])
-        contact = str(request.form['contact'])
-        message = str(request.form['message'])[:500]
-        captcha_response = request.form['g-recaptcha-response']
+    if request.method == "POST":
+        name = str(request.form["name"])
+        email = str(request.form["email"])
+        contact = str(request.form["contact"])
+        message = str(request.form["message"])[:500]
+        captcha_response = request.form["g-recaptcha-response"]
 
     context = {
         "content": github_repos,
         "projects_data": projects_data,
         "cert_data": cert_data,
-        "skill_category": data['categories'],
-        # "sitekey": SITEKEY,
-        "skills_data": data['skills'],
+        "skill_category": data["skills"]["categories"],
+        "skills_data": data["skills"]["skills"],
     }
 
     return render_template("v3.html", **context)
