cb174413af938d20c668b457cbddc39d993088ef
adiagarwalrock <aditya.ag1234@gmail.com>
2025-12-15 21:30:40 -0500

    fix: update portfolio name and compatibility date in wrangler configuration

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,45 +1,47 @@
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [vue()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      },
-      build: {
-        target: 'esnext',
-        minify: 'esbuild',
-        cssCodeSplit: true,
-        chunkSizeWarningLimit: 500,
-        reportCompressedSize: false, // Speed up build
-        rollupOptions: {
-          output: {
-            manualChunks(id) {
-              if (id.includes('node_modules')) {
-                if (id.includes('vue') || id.includes('pinia') || id.includes('router')) {
-                    return 'vendor-core';
-                }
-                return 'vendor-libs';
+  const env = loadEnv(mode, '.', '');
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [vue()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    },
+    base: "/",
+    build: {
+      target: 'esnext',
+      minify: 'esbuild',
+      outDir: "dist",
+      cssCodeSplit: true,
+      chunkSizeWarningLimit: 500,
+      reportCompressedSize: false, // Speed up build
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('vue') || id.includes('pinia') || id.includes('router')) {
+                return 'vendor-core';
               }
-            },
-            entryFileNames: 'assets/[name].[hash].js',
-            chunkFileNames: 'assets/[name].[hash].js',
-            assetFileNames: 'assets/[name].[hash].[ext]'
-          }
+              return 'vendor-libs';
+            }
+          },
+          entryFileNames: 'assets/[name].[hash].js',
+          chunkFileNames: 'assets/[name].[hash].js',
+          assetFileNames: 'assets/[name].[hash].[ext]'
         }
       }
-    };
+    }
+  };
 });
